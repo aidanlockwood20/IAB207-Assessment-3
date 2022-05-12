3b830797f29d474e20bd3d56700804c52248cfd1
aidanlockwood20 <aidanlockwood@yahoo.com.au>
2022-05-12 10:14:30 +1000

    Initialised Database for Application

--- landingPage/__init__.py
+++ landingPage/__init__.py
@@ -3,3 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 # Initialising the database object
 db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    # App Configurations
+    app.debug = True
+    app.secret_key = 'Somesecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///backyardCinemas.db'
+
+    # Initialising the database
+    db.init_app(app)
+
+    return app
