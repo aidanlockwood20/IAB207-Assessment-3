792976eb3e23e9a56dc880141bf619464e248756
aidanlockwood20 <aidanlockwood@yahoo.com.au>
2022-05-17 16:54:07 +1000

    Create Login and Registration Forms

---
+++ backyardCinemas/forms/auth_forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import InputRequired, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+
+    first_name = StringField('First Name', validators = [InputRequired()])
+    last_name = StringField('Last Name', validators = [InputRequired()])
+    email_address = StringField('Email Address', validators = [InputRequired(), Email()])
+    password1 = PasswordField('Password', validators = [InputRequired()])
+    password2 = PasswordField('Confirm Password', validators = [InputRequired(), EqualTo('password1', message = 'Passwords Must Match!')])
+    submit = SubmitField('Sign Up!')
+
+class LoginForm(FlaskForm):
+
+    email_address = StringField('Email Address', validators = [InputRequired()])
+    password = PasswordField('Password', validators = [InputRequired()])
+    submit = SubmitField('Login')
