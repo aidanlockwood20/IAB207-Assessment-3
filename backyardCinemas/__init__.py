3e626285aaaef0fa674d19ce09fdc4cce51c7318
aidanlockwood20 <aidanlockwood@yahoo.com.au>
2022-05-19 16:23:25 +1000

    Add Login Manager

--- backyardCinemas/__init__.py
+++ backyardCinemas/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 # Initialise DB Object
 db = SQLAlchemy()
@@ -10,10 +12,18 @@ def create_app():
     # Initialise new Flask app object
     app = Flask(__name__)
 
+    bootstrap = Bootstrap(app)
+
     # Configure app object
     app.debug = True
     app.secret_key = 'Somesecretkey'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///backyardCinemas.db'
+
+    # Configuring the Login Manager
+    login_manager = LoginManager()
+
+    login_manager.login_view = 'auth_bp.login_view'
+    login_manager.init_app(app)
 
     db.init_app(app)
 
