63fd95b498dbe83b146ed6f051fdd44fe1c5b8c2
aidanlockwood20 <aidanlockwood@yahoo.com.au>
2022-05-19 15:59:01 +1000

    Registering the authentication blueprint

--- backyardCinemas/__init__.py
+++ backyardCinemas/__init__.py
@@ -17,8 +17,14 @@ def create_app():
 
     db.init_app(app)
 
+    # importing the main blueprint
     from .landing_page_views import main_bp
     app.register_blueprint(main_bp)
+    
+    # importing the authentication blueprint
+    from .auth_views import auth_bp
+    app.register_blueprint(auth_bp)
+
     from .error_views import page_not_found
     app.register_error_handler(404, page_not_found)
 
