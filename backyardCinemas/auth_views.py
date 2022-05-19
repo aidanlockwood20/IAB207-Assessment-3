c2fdb3b2e262407944fbe699352396126fbc3952
aidanlockwood20 <aidanlockwood@yahoo.com.au>
2022-05-19 15:50:55 +1000

    Added Flash Messages to views

--- backyardCinemas/auth_views.py
+++ backyardCinemas/auth_views.py
@@ -2,8 +2,8 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user
-
 from . import db
+
 # Form and Model imports
 from forms.auth_forms import RegistrationForm, LoginForm
 from auth_models import User
@@ -19,6 +19,7 @@ def registration_view():
     form = RegistrationForm()
 
     if form.validate_on_submit():
+        # Code to check if the if conditional is working 
         print('Form submit')
 
         # Gather the data from the form
@@ -37,39 +38,59 @@ def registration_view():
         db.session.add(user)
         db.session.commit()
 
-        # log the user in 
+        # log the user in and show flash message
         authenticate_user = User.query.filter_by(email_address = email_address)
+        flash('Welcome to backyardCinemas!')
         login_user(authenticate_user)
 
     return render_template('auth/authenticate.html', form = form)
 
+# The view to login the user
 @auth_bp.route('/login', methods = ['GET', 'POST'])
 def login_view():
 
+    # Form used to login the user
     form = LoginForm()
+    
+    # Default error value 
+    error = None
 
     if form.validate_on_submit():
 
+        # Debug code to check whether the if conditional works 
         print('Form submit')
 
+        # Get form data
         email_address = form.email_address.data
         password = form.password.data
 
+        # Check db for user
         user_query = User.query.filter_by(email_address = email_address).first()
 
+        # if user is none, assume invalid email address
         if user_query is None:
             error = 'Invalid Email Address. Try Again'
+        # If user is correct but password incorrect, return invalid password
         elif not check_password_hash(user_query.password_hash, password):
             error = 'Incorrect Password. Try Again'
+        # No errors - log user in 
         if error is None:
             login_user(user_query)
+            
+            # Display message to user and redirect to homepage
+            flash('You have been logged in successfully!')
+            return redirect(url_for('main.index'))
+
+        # An error has occurred - display to user
         print(error)
         flash(error)
 
     return render_template('auth/authenticate.html', form = form)
 
+# Logout user 
 @auth_bp.route('/logout', methods = ['GET', 'POST'])
 def logout_view():
 
     logout_user()
+    flash('You have been Successfully Logged Out')
     return redirect(url_for('auth.register_view'))
