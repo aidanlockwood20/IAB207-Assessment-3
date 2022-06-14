c0c565d870504b96160403e15737cf59adab1214
gmmartin01 <105282853+gmmartin01@users.noreply.github.com>
2022-06-14 19:26:57 +1000

    Update event_forms.py
    Forgot to import TimeField (updated).

--- backyardCinemas/event_forms.py
+++ backyardCinemas/event_forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, DateTimeField, IntegerField
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, DateTimeField, IntegerField, TimeField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 from flask_wtf.file import FileRequired, FileField, FileAllowed
 
